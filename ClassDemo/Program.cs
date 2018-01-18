999fdbc58ccad619d122714084f39699f1136e61
Johnny Olson <john.olson3@gmail.com>
2018-01-18 14:42:21 -0800

    Initial commit

---
+++ ClassDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClassDemo.Math;
+
+
+namespace ClassDemo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+        }
+    }
+}
