ab59cdbde00742f9385dd7224ef4683f8f69bf44
Johnny Olson <john.olson3@gmail.com>
2018-01-18 17:46:32 -0800

    Conditional exercises

--- ClassDemo/Program.cs
+++ ClassDemo/Program.cs
@@ -2,60 +2,98 @@
 using ClassDemo.Math;
 
 
-namespace Conditionals
+namespace Excersizes
 {
 
     class Program
     {
         static void Main(string[] args)
         {
-            // If statements
-            int hour = 12;
+            // Get number from user. Print 'valid' if it's between 1-10 and 'invalid' otherwise
+            Console.WriteLine("Enter a number between 1 and 10: ");
+            var input = int.Parse(Console.ReadLine());
+            
 
-            if (hour > 0 && hour < 12)
+            if (input > 0 && input <= 10)
             {
-                Console.WriteLine("It's morning");
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-                Console.WriteLine("It's afternoon");
+                Console.WriteLine("Valid");
             }
             else
             {
-                Console.WriteLine("It's evening");
+                Console.WriteLine("Invalid");
             }
 
 
-            // Ternary statement
-            var isGoldCustomer = true;
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
-            // Same as:
-            // if (isGoldCustomer)
-            //     price = 19.95f;
-            // else
-            //     price = 29.95f;
+            // Get 2 numbers, display larger 
+            Console.WriteLine("Please enter a number: ");
+            var num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another number: ");
+            var num2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(price);
-
-
-            // Switch statements
-            var season = Season.Autumn;
-
-            switch (season)
+            if (num1 > num2)
             {
-                case Season.Autumn:
-                    Console.WriteLine("It's Autumn and beautiful");
-                    break;
-                case Season.Summer:
-                    Console.WriteLine("It's a great day for the beach");
-                    break;
-                case Season.Spring:
-                case Season.Winter:
-                    Console.WriteLine("We've got a promotion!");
-                    break;
-                default:
-                    Console.WriteLine("I don't understand that season");
-                    break;
+                Console.WriteLine(string.Format("{0} is bigger than {1}", num1, num2));
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine(string.Format("{0} is bigger than {1}", num2, num1));
+            }
+            else
+            {
+                Console.WriteLine("The numbers are the same");
+            }
+
+
+            // Get width and height from the user and print 'landscape' or 'portrait'
+            Console.WriteLine("Please enter the width of a picture: ");
+            var width = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the height of the picture: ");
+            var height = int.Parse(Console.ReadLine());
+
+            if (width > height)
+            {
+                Console.WriteLine("This picture is a landscape");
+            }
+            else if (width < height)
+            {
+                Console.WriteLine("This picture is a portrait");
+            }
+            else
+            {
+                Console.WriteLine("This picture is perfectly square!");
+            }
+
+
+            // Speed camera exercise. Ask user for speed limit. Ask for speed of car.
+            // Display 'ok' if under the speed limit. If it's over the speed limit display
+            // the number of demerits. 1 demerit per 5km/h. Over 12 demerits, display 
+            // 'License Suspended'
+
+            Console.WriteLine("Enter the speed Limit: ");
+            var speedLimit = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the speed of the car: ");
+            var carSpeed = int.Parse(Console.ReadLine());
+
+            const int demeritScale = 5;
+
+            if (carSpeed <= speedLimit)
+            {
+                Console.WriteLine("Speed OK!");
+            }
+            else
+            {
+                var speedOverLimit = carSpeed - speedLimit;
+                var numDemerits = speedOverLimit / demeritScale;
+                numDemerits += 1;   // 1-5 over should give a demerit
+
+                if (numDemerits > 12)
+                {
+                    Console.WriteLine("License Suspended");
+                }
+                else
+                {
+                    Console.WriteLine("You have received " + numDemerits + " demerits!");
+                }
             }
         }
     }
